54cc87919f1daae1d04ba6acc0fc517ca293fc83
ashtneoi <ashtneoi@gmail.com>
2019-05-18 19:43:54 -0700

    Rename stuff, improve tests, and add EventOutput

--- eventual.py
+++ eventual.py
@@ -4,17 +4,92 @@ import unittest
 from functools import update_wrapper
 
 
-class EventInputCreator:
+class Creator:
+    def __set_name__(self, owner, name):
+        self.name = name
+
+
+class Input:
+    def __init__(self, actor, f):
+        self.actor = actor
+        self.f = f
+
+        self.up = None
+
+    def __call__(self, thing):
+        self.f(self.actor, thing)
+
+
+class EventInputInstance(Input):
+    def attach(self, up):
+        if not isinstance(up, EventOutputInstance):
+            raise Exception(
+                f"Can't connect {type(self)} to {type(up)}"
+            )
+        up.down.append(self)
+        self.up = up
+
+
+class EventInput(Creator):
     def __init__(self, f):
         self.f = f
 
+    def __get__(self, instance, owner):
+        # Create instance attribute to override us.
+        obj = EventInputInstance(instance, self.f)
+        setattr(instance, self.name, obj)
+        return obj
 
-class ValueInputCreator:
+
+class ValueInputInstance(Input):
+    def attach(self, up):
+        if not isinstance(up, ValueOutputInstance):
+            raise Exception(
+                f"Can't connect {type(self)} to {type(up)}"
+            )
+        up.down.append(self)
+        self.up = up
+
+
+class ValueInput(Creator):
     def __init__(self, f):
         self.f = f
 
+    def __get__(self, instance, owner):
+        # Create instance attribute to override us.
+        obj = ValueInputInstance(instance, self.f)
+        setattr(instance, self.name, obj)
+        return obj
 
-class Sync:
+
+class EventOutputInstance:
+    def __init__(self, actor):
+        self.actor = actor
+
+        self.down = []
+
+    def attach(self, down):
+        if not isinstance(up, EventInputInstance):
+            raise Exception(
+                f"Can't connect {type(self)} to {type(up)}"
+            )
+        down.up = self
+        self.down.append(down)
+
+    def __call__(self, ev):
+        for d in self.down:
+            d(ev)
+
+
+class EventOutput(Creator):
+    def __get__(self, instance, owner):
+        # Create instance attribute to override us.
+        obj = EventOutputInstance(instance)
+        setattr(instance, self.name, obj)
+        return obj
+
+
+class SyncInstance:
     def __init__(self, actor, f):
         self.actor = actor
         self.f = f
@@ -28,11 +103,15 @@ class Sync:
     def attach(self, peer):
         if self.peer is not None:
             raise Exception("Can only attach once")
+        if not isinstance(peer, SyncInstance):
+            raise Exception(
+                f"Can't connect {type(self)} to {type(peer)}"
+            )
         peer.peer = self
         self.peer = peer
 
 
-class SyncCreator:
+class Sync(Creator):
     def __init__(self, f):
         self.f = f
 
@@ -41,25 +120,25 @@ class SyncCreator:
 
     def __get__(self, instance, owner):
         # Create instance attribute to override us.
-        obj = Sync(instance, self.f)
+        obj = SyncInstance(instance, self.f)
         setattr(instance, self.name, obj)
         return obj
 
 
 def event_input(f):
-    e = EventInputCreator(f)
+    e = EventInput(f)
     update_wrapper(e, f)
     return e
 
 
 def value_input(f):
-    e = ValueInputCreator(f)
+    e = ValueInput(f)
     update_wrapper(e, f)
     return e
 
 
 def sync(f):
-    e = SyncCreator(f)
+    e = Sync(f)
     update_wrapper(e, f)
     return e
 
@@ -80,7 +159,31 @@ class Event:
 
 
 class Test(unittest.TestCase):
-    def test(self):
+    def test_event_input_output(self):
+        class Thing(Actor):
+            trigger = EventOutput()
+
+            def __init__(self):
+                self.a = 0
+
+            @event_input
+            def hey(self, ev):
+                self.a = ev.data
+
+        x = Thing()
+        y = Thing()
+        x.attach(hey=y.trigger)
+
+        self.assertEqual(x.a, 0)
+        self.assertEqual(y.a, 0)
+        x.trigger(Event(100))
+        self.assertEqual(x.a, 0)
+        self.assertEqual(y.a, 0)
+        y.trigger(Event(200))
+        self.assertEqual(x.a, 200)
+        self.assertEqual(y.a, 0)
+
+    def test_sync(self):
         class Thing(Actor):
             def __init__(self):
                 self.a = 0
